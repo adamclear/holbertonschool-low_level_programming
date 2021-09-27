cbd32c1af105daec2bb3339f9c366da4d86b1a5c
Adam Clear <3589@holbertonschool.com>
2021-09-27 19:12:29 +0000

    added file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* This file prints a string followed by a new line, to the standard output. */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
+}
