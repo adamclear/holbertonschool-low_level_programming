801dcfacafee346e514a05e5fc5e80a5284f8f31
Adam Clear <3589@holbertonschool.com>
2021-09-27 19:22:20 +0000

    corrected file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /* This file prints a string followed by a new line, to the standard output. */
-
+/**
+ * main - Prints a string to standard output
+ *
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
