9212165d979c0d2846f6838fa08ce36b19f8a603
Adam Clear <3589@holbertonschool.com>
2021-09-28 16:40:16 +0000

    added file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* prints the alphabet in lower case */
+/**
+ * main- prints the alphabet in lower case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
