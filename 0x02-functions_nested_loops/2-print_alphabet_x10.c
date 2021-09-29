4c1bff72cde3cf42c8002dbd21b1e95d46c96959
Adam Clear <3589@holbertonschool.com>
2021-09-29 17:27:34 +0000

    added file 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints the alphabet 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+int i = 0, j;
+char c = 'a';
+while (i <= 10)
+{
+j = 0;
+while (j < 1)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
+
+return;
+}
