ce28a9f800dc58ef02757a3900ecee8bdad329cc
Adam Clear <3589@holbertonschool.com>
2021-09-29 17:09:57 +0000

    added file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- prints the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char c = 'a';
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return;
+}
