978453ef1c7ca1abc6736b1ad29f7e3dfede8d12
Adam Clear <3589@holbertonschool.com>
2021-10-05 15:54:51 +0000

    added file 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev- prints a string in reverse
+ * @s: input string pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	while (l >= 0)
+	{
+		_putchar(*s);
+		s--;
+		l--;
+	}
+	_putchar('\n');
+return;
+}
