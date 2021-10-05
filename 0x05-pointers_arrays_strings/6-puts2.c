69bf2fca73bc2d0d0507d746aff810c4defa08e5
Adam Clear <3589@holbertonschool.com>
2021-10-05 17:51:12 +0000

    corrected file 6, betty style correction and added check for end of string character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,19 @@
  */
 void puts2(char *str)
 {
-    while (*str != '\0')
-    {
-        _putchar(*str);
-        str = str + 2;
-    }
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (*str == '\0')
+		{
+			break;
+		}
+		else
+		{
+			str++;
+		}
+	}
 	_putchar('\n');
 return;
 }
