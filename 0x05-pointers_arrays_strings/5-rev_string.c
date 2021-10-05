a127bf5da88e480dfdd26daa1a5bbd17e92621c1
Adam Clear <3589@holbertonschool.com>
2021-10-05 17:22:04 +0000

    added file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * rev_string- reverses a string
+ * @s: input variable pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char *ptrb, *ptre, ch;
+
+	l = _strlen(s);
+
+	ptrb = s;
+	ptre = s;
+
+	for (i = 0; i < l - 1; i++)
+	{
+		ptre++;
+	}
+
+	for (i = 0; i < l / 2; i++)
+	{
+		ch = *ptre;
+		*ptre = *ptrb;
+		*ptrb = ch;
+
+		ptrb++;
+		ptre--;
+	}
+return;
+}
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+		{
+			l++;
+			s++;
+		}
+	return (l);
+}
