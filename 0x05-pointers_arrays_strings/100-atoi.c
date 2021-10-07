db485dcb0ff34d2961e0f278dfa0ba89ccaaa6d4
Adam Clear <3589@holbertonschool.com>
2021-10-07 19:40:09 +0000

    corrected file 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,17 @@ int _atoi(char *s)
 {
 	int val = 0;
 	int neg = 0;
-	int pos = 0;
-	int sign;
+	int sign = 1;
 
 	while (*s != '\0')
 	{
+		if (*s >= '0' && *s <= '9')
+		{
+			break;
+		}
 		if (*s == '-')
 		{
 			neg++;
-		}
-		else if (*s == '+')
-		{
-			pos++;
-		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			val = val * 10 + (*s - '0');
-		}
-		else if (*s == ';')
-		{
-			break;
 		}
 		s++;
 	}
@@ -35,9 +26,17 @@ int _atoi(char *s)
 	{
 		sign = -1;
 	}
-	else
+	while (*s != '\0')
 	{
-		sign = 1;
+		if (*s >= '0' && *s <= '9')
+		{
+			val = val * 10 + ((*s - '0') * sign);
+		}
+		else if(*s < '0' || *s > '9')
+		{
+			break;
+		}
+		s++;
 	}
-return (sign * val);
+return (val);
 }
