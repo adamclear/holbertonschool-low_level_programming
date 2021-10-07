d188a69a8c5eea745494f94d6ca32aef30f46921
Adam Clear <3589@holbertonschool.com>
2021-10-07 15:09:52 +0000

    added file 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _atoi- converts a string to an integer
+ * @s: input variable pointer
+ * Return: sign * val
+ */
+int _atoi(char *s)
+{
+	int val = 0;
+	int neg = 0;
+	int pos = 0;
+	int sign;
+
+	while (*s == '\0')
+	{
+		if (*s == '-')
+		{
+			neg++;
+		}
+		else if (*s == '+')
+		{
+			pos++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			val = val * 10 + *s - '0';
+		}
+		s++;
+	}
+	if (neg > pos)
+	{
+		sign = -1;
+	}
+	else
+	{
+		sign = 1;
+	}
+return (sign * val);
+}
