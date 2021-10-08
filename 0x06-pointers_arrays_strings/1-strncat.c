87c3a682fed6ba6fa8a6205e721d9f1c7f357465
Adam Clear <3589@holbertonschool.com>
2021-10-08 18:31:55 +0000

    added file 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strncat- concatenates two strings
+ * @dest: destination array pointer
+ * @src: source array pointer
+ * @n: limiting variable
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lend = 0;
+	int lens;
+
+	while (*dest !='\0')
+	{
+		dest++;
+		lend++;
+	}
+	for (lens = 0; lens < n; lens ++)
+	{
+		if (*src == '\0')
+		{
+			break;
+		}
+		else
+		{
+			*(dest++) = *(src++);
+		}
+	}
+	dest = dest - (lend + lens);
+return (dest);
+}
