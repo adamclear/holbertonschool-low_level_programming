df8ab96a4fb18e2203e66c9993c691c1c854d78a
Adam Clear <3589@holbertonschool.com>
2021-10-08 21:22:05 +0000

    added file 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strncpy- copies a string
+ * @dest: destination array pointer
+ * @src: source array pointer
+ * @n: input variable length of copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	for (len = 0; len < n; len++)
+	{
+		*(dest++) = *(src++);
+	}
+	dest = dest - len;
+return (dest);
+}
