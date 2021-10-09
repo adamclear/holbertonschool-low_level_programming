697d745a9a5ed05e2d67e5f1d7a05ae72ddce895
Adam Clear <3589@holbertonschool.com>
2021-10-09 20:16:41 +0000

    added file 4

---
+++ 0x06-pointers_arrays_strings/rev_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * reverse_array- reverses an array of integers
+ * @a: input array pointer
+ * @n: number of positions to swap
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int l = 0;
+	int tmp;
+	int *beg = a;
+	int *end = a;
+
+	while (l < n - 1)
+	{
+		end++;
+		l++;
+	}
+	l = 0;
+	for (l = 0; l < n / 2; l++)
+	{
+		tmp = *end;
+		*end = *beg;
+		*beg = *tmp;
+
+		beg++;
+		end--;
+	}
+return;
+}
