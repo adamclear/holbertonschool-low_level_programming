948eba426cba455120f6497e3f94eec35abadba2
Adam Clear <3589@holbertonschool.com>
2021-10-11 17:37:01 +0000

    added file 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr- locates the first instance of a character in a string
+ * @s: input string pointer
+ * @c: input character to be located
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int l;
+
+	for (l = 0; *s != '\0'; l++)
+	{
+		if (*s == c)
+		{
+			break;
+		}
+		s++;
+	}
+return (s);
+}
