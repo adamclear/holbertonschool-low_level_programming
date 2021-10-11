a0aa4a34dfdb19a36130e31148920144269b36f5
Adam Clear <3589@holbertonschool.com>
2021-10-11 23:50:40 +0000

    added file 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strstr- locates a substring
+ * @haystack: input array pointer
+ * @needle: substring array pointer
+ * Return: needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int l;
+	int n;
+
+	for (l = 0; *haystack != '\0'; l++)
+	{
+		if (*haystack == *needle)
+		{
+			for (n = 0; *needle != '\0'; n++)
+			{
+				needle++;
+				haystack++;
+				if (*haystack == *needle)
+				{
+					continue;
+				}
+				else if (*haystack != *needle)
+				{
+					needle = needle - n;
+					haystack = haystack - n;
+					break;
+				}
+			}
+		}
+		if (*needle == '\0')
+		{
+			needle = needle - n;
+			return (needle);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
