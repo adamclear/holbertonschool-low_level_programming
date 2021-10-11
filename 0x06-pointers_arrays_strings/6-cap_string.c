886646be16f05cb8c230d69d3fa505bf4a6828be
Adam Clear <3589@holbertonschool.com>
2021-10-11 01:52:53 +0000

    corrected file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,14 @@ char *cap_string(char *str)
 {
 	int l;
 	int i = 0;
-	char sep[13] = {32, 9, '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	char sep[14] = {32, 9, '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125, '\0'};
 	char *psep = sep;
 
 	for (l = 0; *str != '\0'; l++)
 	{
 		if (*str >= 'a' && *str <= 'z' && l > 0)
 		{
-			while (i < 13)
+			while (i < 14)
 			{
 				if (*(str - 1) == *psep)
 				{
