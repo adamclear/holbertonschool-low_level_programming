8cfc0aeccaae630d24769650a0f3cbb3022d90a9
Adam Clear <3589@holbertonschool.com>
2021-10-18 22:20:06 +0000

    added file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * mynameis- program prints its own name
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments passed
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+return (0);
+}
