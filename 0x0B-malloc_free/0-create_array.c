df75e37fd9e845eb6799566cdd4fc87a087410dd
Adam Clear <3589@holbertonschool.com>
2021-10-20 16:37:08 +0000

    created 0x0B dir, README, main.h, _putchar.c, file 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array- allocates memory for a character array of a dictated size and
+ * populates the array with the dictated character.
+ * @size: variable for size of the array
+ * @c: variable for the character
+ * Return: buffer if successfull, NULL if not or if size is 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer = (char *)malloc(size * sizeof(char));
+	unsigned int x;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+	else if (size > 0)
+	{
+		for (x = 0; x <= size; x++)
+		{
+			buffer[x] = c;
+		}
+		return (buffer);
+	}
+return (NULL);
+}
