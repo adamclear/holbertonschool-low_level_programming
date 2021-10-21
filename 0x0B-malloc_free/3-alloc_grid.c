78e44e7d09f2c3a270d43ee6a6084e1c454a100f
Adam Clear <3589@holbertonschool.com>
2021-10-21 02:59:40 +0000

    added file 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * alloc_grid- creates a grid based on input variables and populates with 0
+ * @width: input variable int
+ * @height: input variable int
+ * Return: grid on success, NULL if not
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid = (int *)malloc(height * sizeof(int *));
+	int x;
+	int y;
+
+	if (grid == NULL || width == NULL || height == NULL)
+		return (NULL);
+
+	for (y = 0; y < height; y++)
+	{
+		grid[y] = (int *)malloc(width * sizeof(int));
+		for (x = 0; x < width; x++)
+		{
+			gird[y][x] = 0;
+		}
+	}
+return (grid);
+}
