1e29ea7a2a5f2830550c2ff11670faf540a730a1
Adam Clear <3589@holbertonschool.com>
2021-10-26 19:52:13 +0000

    corrected file 2, enclosed in if true cond

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if (_strlen(d->name) == 0)
-		d->name = "(nil)";
-
-	if (_strlen(d->owner) == 0)
-		d->owner = "(nil)";
 	if (d)
 	{
+		if (_strlen(d->name) == 0)
+			d->name = "(nil)";
+
+		if (_strlen(d->owner) == 0)
+			d->owner = "(nil)";
+
 		printf("Name: %s\n", d->name);
 		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner);
