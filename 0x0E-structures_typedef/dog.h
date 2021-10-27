4238e2dd2d00e7d1b11cb139aa379885fcfb128b
Adam Clear <3589@holbertonschool.com>
2021-10-27 06:52:30 +0000

    cleanup

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - datatype for the name, age, and owner of a dog
@@ -19,6 +19,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+char *_strdup(char *src);
 
 #endif
