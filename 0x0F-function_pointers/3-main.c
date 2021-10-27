b10861694accbafda138b464f4644f3ec16f08a7
Adam Clear <3589@holbertonschool.com>
2021-10-27 08:38:52 +0000

    updated task 3 files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,6 +5,14 @@
  * main- uses arguments passed to do math
  * Return: 0 on success
  */
-int main();
+int main(int argc, char *argv[])
 {
+	int a = atoi(argv[0]);
+	int b = atoi(argv[3]);
+	char op = argv[2];
+	int ans;
+
+	ans = get_op_func(op, a, b);
+
+	return (0);
 }
