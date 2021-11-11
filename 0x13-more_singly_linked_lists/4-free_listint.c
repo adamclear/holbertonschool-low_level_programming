dbed2882fca393ae591325a5ebbc6000a6d6fc64
Adam Clear <3589@holbertonschool.com>
2021-11-11 22:28:06 +0000

    added file 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: input pointer to head of list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *nextnode;
+
+	while (head)
+	{
+		nextnode = head->next;
+		free(head->n);
+		free(head);
+		head = nextnode;
+	}
+	return;
+}
