87a2ba91ad71e23f4c1ef191f31c29cfa133e047
Adam Clear <3589@holbertonschool.com>
2021-11-12 03:33:53 +0000

    added file 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "limits.h"
+/**
+ * sum_listint - sums the ints in all nodes of a linked list
+ * @head: input pointer to head node
+ * Return: the sum of all ints in the nodes
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+	{
+		return (0);
+	}
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+return (sum);
+}
