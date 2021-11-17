af79b4e3d79b81eed8b05f9ba6c6366a060089aa
Adam Clear <3589@holbertonschool.com>
2021-11-17 23:43:24 +0000

    added empty text to add if text_content is empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (openit == -1)
 		return (-1);
 	/* if there is text content, write it to the file */
+	if (!text_content)
+		text_content = "";
 	if (text_content)
 		write(openit, text_content, x);
 return (1);
