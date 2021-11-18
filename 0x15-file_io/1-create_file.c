92f14ee47c01296519242d58a179b12759ccf4b0
Adam Clear <3589@holbertonschool.com>
2021-11-18 00:06:05 +0000

    corrected file 1, removed redundant check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
 	/* if there is text content, write it to the file */
 	if (!text_content)
 		text_content = "";
-	if (text_content)
-		write(openit, text_content, x);
+	write(openit, text_content, x);
 return (1);
 }
