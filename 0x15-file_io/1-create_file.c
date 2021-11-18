310a2f71329a2efc592c2f6db525137009316bcd
Adam Clear <3589@holbertonschool.com>
2021-11-18 00:09:13 +0000

    corrected file 1, removed assignment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int openit;
-	int x = strlen(text_content);
+	int x;
 
 	/* check for filename and if file can be opened/created */
 	if (!filename)
@@ -19,6 +19,6 @@ int create_file(const char *filename, char *text_content)
 	/* if there is text content, write it to the file */
 	if (!text_content)
 		text_content = "";
-	write(openit, text_content, x);
+	write(openit, text_content, strlen(text_content));
 return (1);
 }
