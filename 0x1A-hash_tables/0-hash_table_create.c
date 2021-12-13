ff53f2d9f36e1bcc404e137703026a30ebc76310
Adam Clear <3589@holbertonschool.com>
2021-12-13 03:25:54 +0000

    corrected file 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int x;
 	hash_table_t *hashtable = malloc(sizeof(hash_table_t));
 
+	if (!hashtable)
+	{
+		free(hashtable);
+		return (NULL);
+	}
 	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (hashtable && hashtable->array)
+	if (!hashtable->array)
 	{
-		for (x = 0; x < size; x++)
-			hashtable->array[x] = NULL;
-		return (hashtable);
+		free(hashtable->array);
+		free(hashtable);
+		return (NULL);
 	}
-	return (NULL);
+	for (x = 0; x < size; x++)
+		hashtable->array[x] = NULL;
+	return (hashtable);
 }
