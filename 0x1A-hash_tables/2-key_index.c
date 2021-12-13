190dc905eb21fa9c483ee8326652b12dee2ada19
Adam Clear <3589@holbertonschool.com>
2021-12-13 01:49:48 +0000

    bettyfied

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,7 @@
  * key_index - finds the index of a particular key
  * @key: the key
  * @size: the size of the table
+ * Return: the index of the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
