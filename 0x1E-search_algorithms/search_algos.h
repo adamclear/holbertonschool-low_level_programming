cbb53defd72d84b3e3894d71555bae90b40c02a4
adamclear <3589@holbertonschool.com>
2022-06-16 20:28:26 +0000

    task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 
+int binary_search(int *array, size_t size, int value);
+int recursive_binary_search(int *array, int start, int end, int value);
+void print_partial(int *array, int index, int end);
+
 #endif
