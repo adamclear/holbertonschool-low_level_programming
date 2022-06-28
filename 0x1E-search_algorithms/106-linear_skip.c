be6d6ec268c6a71b2b09cefb81a90a6114a34f2f
adamclear <3589@holbertonschool.com>
2022-06-28 06:44:42 +0000

    fixed betty up good

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -9,48 +9,37 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *next_exp, *cur_node = list, *next_node;
 
-	/* Check if head is NULL or if node->n is greater than value */
 	if (!list || cur_node->n > value)
 		return (NULL);
-	/* Else start checking value against current node */
-	/* and next express node						  */
-	else
+	for (; cur_node; cur_node = cur_node->express)
 	{
-		/* Iterate through express nodes checking value against node->n */
-		for (; cur_node; cur_node = cur_node->express)
+		next_exp = cur_node->express;
+		printf("Value checked at index [%ld] = [%d]\n",
+				next_exp->index, next_exp->n);
+		if ((cur_node->n <= value && next_exp->n >= value) ||
+				next_exp->express == NULL)
 		{
-			next_exp = cur_node->express;
-			printf("Value checked at index [%ld] = [%d]\n",
-					next_exp->index, next_exp->n);
-			/* If value is btwn node->n & express->n */
-			if ((cur_node->n <= value && next_exp->n >= value) ||
-					next_exp->express == NULL)
+			if (next_exp->express == NULL)
 			{
-				if (next_exp->express == NULL)
-				{
-					cur_node = next_exp;
-					for (; next_exp->next;)
-						next_exp = next_exp->next;
-				}
+				cur_node = next_exp;
+				for (; next_exp->next;)
+					next_exp = next_exp->next;
+			}
+			next_node = cur_node->next;
+			printf("Value found between indexes [%ld] and [%ld]\n",
+					cur_node->index, next_exp->index);
+			for (; next_node->index <= next_exp->index;
+					cur_node = cur_node->next)
+			{
 				next_node = cur_node->next;
-				printf("Value found between indexes [%ld] and [%ld]\n",
-						cur_node->index, next_exp->index);
-				/* Check for value up to epress->index */
-				for (; next_node->index <= next_exp->index;
-						cur_node = cur_node->next)
-				{
-					next_node = cur_node->next;
-					printf("Value checked at index [%ld] = [%d]\n",
-							cur_node->index, cur_node->n);
-					if (cur_node->n == value)
-						return (cur_node);
-					/* Value not in list */
-					else if (cur_node->index == next_exp->index)
-						return (NULL);
-				}
+				printf("Value checked at index [%ld] = [%d]\n",
+						cur_node->index, cur_node->n);
+				if (cur_node->n == value)
+					return (cur_node);
+				else if (cur_node->index == next_exp->index)
+					return (NULL);
 			}
 		}
-		/* If all express nodes are checked and value still not found */
-		return (NULL);
 	}
+	return (NULL);
 }
