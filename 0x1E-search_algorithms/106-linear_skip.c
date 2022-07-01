74dc3c649b83fadb43fc351e65a19940733fcf1e
adamclear <3589@holbertonschool.com>
2022-07-01 19:24:21 +0000

    updated

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -16,8 +16,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		next_exp = cur_node->express;
 		printf("Value checked at index [%ld] = [%d]\n",
 				next_exp->index, next_exp->n);
-		if ((cur_node->n <= value && next_exp->n >= value) ||
-				next_exp->express == NULL)
+		if (next_exp->n >= value || next_exp->express == NULL)
 		{
 			if (next_exp->express == NULL)
 			{
